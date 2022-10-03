6dfef354ea9399d719201928aaed831e133c44b5
fernandocevelicek <cevelicekfernando@gmail.com>
2022-10-03 18:35:28 -0300

    Agrego Proyecto

---
+++ src/main/java/com/grupo1/FlipFloppin/FlipFloppinApplication.java
@@ -0,0 +1,13 @@
+package com.grupo1.FlipFloppin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipFloppinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipFloppinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo1/FlipFloppin/FlipFloppinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.FlipFloppin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipFloppinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
