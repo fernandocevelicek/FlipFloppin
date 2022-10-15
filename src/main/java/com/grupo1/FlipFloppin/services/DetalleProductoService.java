f83e158c3d1d166122bf3dd406f6f2e35ae34803
Fernando Cevelicek <cevelicekfernando@gmail.com>
2022-10-15 20:00:21 -0300

    Todos los Servicios con Dto

--- src/main/java/com/grupo1/FlipFloppin/services/DetalleProductoService.java
+++ src/main/java/com/grupo1/FlipFloppin/services/DetalleProductoService.java
@@ -1,6 +1,8 @@
 package com.grupo1.FlipFloppin.services;
 
+import com.grupo1.FlipFloppin.dtos.DetalleProductoDTO;
 import com.grupo1.FlipFloppin.entities.DetalleProducto;
+import com.grupo1.FlipFloppin.mappers.DetalleProductoMapper;
 import com.grupo1.FlipFloppin.repositories.DetalleProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,16 +12,18 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class DetalleProductoService implements BaseService<DetalleProducto> {
+public class DetalleProductoService implements BaseService<DetalleProductoDTO> {
 
     @Autowired
     DetalleProductoRepository detalleProductoRepository;
 
+    private DetalleProductoMapper detalleProductoMapper = DetalleProductoMapper.getInstance();
+
     @Override
     @Transactional
-    public List<DetalleProducto> findAll() throws Exception {
+    public List<DetalleProductoDTO> findAll() throws Exception {
         try {
-            List<DetalleProducto> detalleProductos = detalleProductoRepository.findAll();
+            List<DetalleProductoDTO> detalleProductos = detalleProductoMapper.toDTOsList(detalleProductoRepository.findAll());
             return detalleProductos;
         }catch (Exception e){
             throw new Exception(e.getMessage());
@@ -28,11 +32,11 @@ public class DetalleProductoService implements BaseService<DetalleProducto> {
 
     @Override
     @Transactional
-    public DetalleProducto findById(Long id) throws Exception {
+    public DetalleProductoDTO findById(Long id) throws Exception {
         try{
             Optional<DetalleProducto> opt = detalleProductoRepository.findById(id);
             DetalleProducto detalleProducto = opt.get();
-            return detalleProducto;
+            return detalleProductoMapper.toDTO(detalleProducto);
         }catch (Exception e){
             throw new Exception(e.getMessage());
         }
@@ -40,11 +44,12 @@ public class DetalleProductoService implements BaseService<DetalleProducto> {
 
     @Override
     @Transactional
-    public DetalleProducto update(DetalleProducto entity, Long id) throws Exception {
+    public DetalleProductoDTO update(DetalleProductoDTO dto, Long id) throws Exception {
         try{
+            DetalleProducto entity = detalleProductoMapper.toEntity(dto);
             if(detalleProductoRepository.existsById(id)) {
                 DetalleProducto detalleProducto = detalleProductoRepository.save(entity);
-                return detalleProducto;
+                return detalleProductoMapper.toDTO(detalleProducto);
             }
             throw new Exception("No existe un detalle producto con el id: " + id);
         }catch (Exception e){
@@ -54,10 +59,11 @@ public class DetalleProductoService implements BaseService<DetalleProducto> {
 
     @Override
     @Transactional
-    public DetalleProducto save(DetalleProducto entity) throws Exception {
+    public DetalleProductoDTO save(DetalleProductoDTO dto) throws Exception {
         try{
+            DetalleProducto entity = detalleProductoMapper.toEntity(dto);
             DetalleProducto detalleProducto = detalleProductoRepository.save(entity);
-            return detalleProducto;
+            return detalleProductoMapper.toDTO(detalleProducto);
         }catch (Exception e){
             throw new Exception(e.getMessage());
         }
@@ -77,5 +83,4 @@ public class DetalleProductoService implements BaseService<DetalleProducto> {
             throw new Exception(e.getMessage());
         }
     }
-
 }
