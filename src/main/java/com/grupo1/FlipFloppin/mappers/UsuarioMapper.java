446934fac233715b2c977a2f98bd55ae3545b14f
Octavio Fariña <octavio@quintoimpacto.net>
2022-11-09 00:36:32 -0300

    fix recursion dto mapper

--- src/main/java/com/grupo1/FlipFloppin/mappers/UsuarioMapper.java
+++ src/main/java/com/grupo1/FlipFloppin/mappers/UsuarioMapper.java
@@ -2,6 +2,8 @@ package com.grupo1.FlipFloppin.mappers;
 
 import com.grupo1.FlipFloppin.dtos.UsuarioDTO;
 import com.grupo1.FlipFloppin.entities.Usuario;
+import org.mapstruct.Context;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -15,16 +17,36 @@ public interface UsuarioMapper {
         return Mappers.getMapper(UsuarioMapper.class);
     }
 
-    @Mapping(source = "id", target = "id")
-    UsuarioDTO toDTO(Usuario source);
+    @InheritInverseConfiguration
+    UsuarioDTO toDTO(Usuario source, @Context CycleAvoidingMappingContext cycleAvoidingMappingContext);
 
     @Mapping(source = "id", target = "id")
-    Usuario toEntity(UsuarioDTO source);
+    Usuario toEntity(UsuarioDTO source, @Context CycleAvoidingMappingContext cycleAvoidingMappingContext);
+
+    @InheritInverseConfiguration
+    List<UsuarioDTO> toDTOsList(List<Usuario> source, @Context CycleAvoidingMappingContext cycleAvoidingMappingContext);
 
     @Mapping(source = "id", target = "id")
-    List<UsuarioDTO> toDTOsList(List<Usuario> source);
+    List<Usuario> toEntitiesList(List<UsuarioDTO> source, @Context CycleAvoidingMappingContext cycleAvoidingMappingContext);
 
-    @Mapping(source = "id", target = "id")
-    List<Usuario> toEntitiesList(List<UsuarioDTO> source);
+    @DoIgnore
+    default UsuarioDTO toDTO(Usuario source) {
+        return toDTO(source, new CycleAvoidingMappingContext());
+    }
+
+    @DoIgnore
+    default Usuario toEntity(UsuarioDTO source) {
+        return toEntity(source, new CycleAvoidingMappingContext());
+    }
+
+    @DoIgnore
+    default List<UsuarioDTO> toDTOsList(List<Usuario> source) {
+        return toDTOsList(source, new CycleAvoidingMappingContext());
+    }
+
+    @DoIgnore
+    default List<Usuario> toEntitiesList(List<UsuarioDTO> source) {
+        return toEntitiesList(source, new CycleAvoidingMappingContext());
+    }
 
 }
