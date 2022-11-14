e6c55ad9514c71ada141f7ddc297502ce1795385
Octavio Fariña <octaviofarinia@gmail.com>
2022-11-14 11:48:21 -0300

    trigger deploy test

--- src/main/java/com/grupo1/FlipFloppin/configuration/ImagenProductoResourceHandler.java
+++ src/main/java/com/grupo1/FlipFloppin/configuration/ImagenProductoResourceHandler.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class ImagenProductoResourceHandler implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/imagenes/**").addResourceLocations("file:/imagenes/");
+        registry.addResourceHandler("/imagenes/**").addResourceLocations("file:imagenes/");
     }
 }
